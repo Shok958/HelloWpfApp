9a668e5652438a9d82563481955230fe5ad23f43
Antonio <antonio958@hotmail.com>
2017-08-17 14:10:55 +0200

    Pruebas

---
+++ HelloWpfApp/Greetings.xaml.vb
@@ -0,0 +1,10 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        If rb1.IsChecked = True Then
+            MessageBox.Show("Hello")
+        Else rb2.IsChecked = True
+            MessageBox.Show("Goodbye")
+        End If
+
+    End Sub
+End Class
